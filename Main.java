c25a27174996a4814322097ca0a968d3ee3b0bf4
Tanimura-Takumi <m1m21a30@oit.ac.jp>
2021-07-04 21:59:57 +0900

    Player

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
 public class Main {
   public static void main(String[] args) {
+<<<<<<< HEAD
+    System.out.println("SEMonster!");
+    Player p1 = new Player();
+    p1.drawMonsters();
+    p1.showDeck();
+=======
     System.out.println("SEMONSTER!!");
     Monster monster = new Monster();
-    Player player = new Player();
     System.out.println(monster.toString());
-    player.drawMonsters();
-    player.showDeck();
+>>>>>>> main
   }
 }
