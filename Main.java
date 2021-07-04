90e29092dcfc3752e0ce6bc26b19e9647800b68a
Tanimura <85854079+Tanimura-Takumi@users.noreply.github.com>
2021-07-04 22:12:19 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-<<<<<<< HEAD
     System.out.println("SEMonster!");
     Player p1 = new Player();
     p1.drawMonsters();
     p1.showDeck();
-=======
-    System.out.println("SEMONSTER!!");
-    Monster monster = new Monster();
-    System.out.println(monster.toString());
->>>>>>> main
   }
 }
